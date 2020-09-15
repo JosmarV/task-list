55282e68285eb8ce9c6943f2e04bc4a4ab34bb6d
Josmar <josmarveliz30@gmail.com>
2020-09-15 15:49:56 -0430

    Aqui solo funciona el add y se guarda en el LS

--- main.js
+++ main.js
@@ -6,34 +6,27 @@
 
   //Functions
   var addTask = function () {
-      let task = taskInput.value;
+    let task = taskInput.value;
 
-      if(task === ""){
-          taskInput.setAttribute("placeholder","Add valid task");
-          taskInput.className = "error";
-          return false;
-      }
+    if (task === "") {
+      taskInput.className = "error";
+      taskInput.setAttribute("placeholder","Write a valid task");
+      return false;
+    }
+    taskInput.value = "";
+    if(localStorage.getItem('tasks') === null) {
+      let tasks = [];
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      let tasks = JSON.parse(localStorage.getItem('tasks'));
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
 
-      taskInput.value = "";
-
-      //Guardando valor en el navegador
-      if(localStorage.getItem('tasks') === null){
-        let tasks = [];
-        tasks.push(task);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-      }
-      else{
-        let tasks = JSON.parse(localStorage.getItem('tasks'))
-        tasks.push(task);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-      }
-      
-      for (var i = 0; i <= list.children.length -1; i++) {
-        list.children[i].addEventListener("click", deleteTask);
-      }
-
+    getTask();
   };
-  getTask();
+
   var inputVoid = function () {
     taskInput.className = "";
     taskInput.setAttribute("placeholder","Write a task");
@@ -43,24 +36,6 @@
     this.parentNode.removeChild(this);
   };
 
-  function getTask(){
-    let tasks = JSON.parse(localStorage.getItem('tasks'));
-    
-    for (var i = 0; i < tasks.length; i++) {
-      let task = tasks[i];
-      let content = document.createTextNode(task);
-            //Adding content to link
-            link.appendChild(content);
-            link.setAttribute("href", "#");
-            //Adding link to new task
-            newTask.appendChild(link);
-            //Adding new task to List
-            list.appendChild(newTask);
-
-    }
-  }
-
-  
   //Events
   //Add task
   btnNewTask.addEventListener("click",addTask);
@@ -73,4 +48,21 @@
       list.children[i].addEventListener("click", deleteTask);
     }
 
+    function getTask(){
+      let tasks = JSON.parse(localStorage.getItem('tasks'));
+      list.innerHTML = '';
+      for(let i = 0; i < tasks.length; i++) {
+        let task = tasks[i],
+            newTask = document.createElement('li'),
+            link = document.createElement('a'),
+            content = document.createTextNode(task);
+
+        link.appendChild(content); //Agregando contenido a el enlace
+        link.setAttribute("href","#");
+        newTask.appendChild(link); //Agregando enlace a la lista
+        list.appendChild(newTask); //Agregando lista la lista ordenada 
+      }
+    }
+    
+    getTask();
 }())
